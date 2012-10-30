a23d92db37b10beb09e6096eb1ccce4ed49ef1ad
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-10-30 14:46:51 -0200

    Initial commit.

---
+++ lib/dispatcher-rails.rb
@@ -0,0 +1,7 @@
+require 'dispatcher-rails/version'
+
+module Dispatcher
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/dispatcher-rails/version.rb
@@ -0,0 +1,5 @@
+module Dispatcher
+  module Rails
+    VERSION = '0.0.1'
+  end
+end
