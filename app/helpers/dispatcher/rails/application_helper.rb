c9540cfff773bbb89e950a1f6b7d3599e0c69426
rodrigo brancher <rbrancher@gmail.com>
2012-11-08 19:16:02 -0200

    creates body_tag helper

--- app/helpers/dispatcher/rails/application_helper.rb
+++ app/helpers/dispatcher/rails/application_helper.rb
@@ -2,11 +2,23 @@ module Dispatcher
   module Rails
     module ApplicationHelper
       def dispatcher_tag
+        tag :meta, name: 'page', content: controller_action_string
+      end
+
+      def body_tag(options = {}, &block)
+        options = options.merge(class: controller_action_string)
+        content_tag :body, options do
+          yield
+        end
+      end
+
+    private
+      def controller_action_string
         controller_name = controller.class.name.underscore
         controller_name.gsub!(/\//, "_")
         controller_name.gsub!(/_controller$/, "")
 
-        tag :meta, name: 'page', content: "#{controller_name}##{controller.action_name}"
+        "#{controller_name}##{controller.action_name}"
       end
     end
   end
