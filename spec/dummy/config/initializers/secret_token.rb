7a99e7cd685c5abafea35058890f61a619d4c29d
Tiago Godinho <tiagogodinho3@gmail.com>
2014-03-01 11:45:07 -0300

    Remove the secret_token deprecation

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'e40912e44be993f30695413b9afe02d1bf7a6f32a6a4a59ee8a414bcf6641c66a5f07371133f22fc268ae029bf563b87e323e5f0b54f667eba7532f10caa0eb3'
+if Rails.version.start_with? '4'
+  Dummy::Application.config.secret_key_base = 'e40912e44be993f30695413b9afe02d1bf7a6f32a6a4a59ee8a414bcf6641c66a5f07371133f22fc268ae029bf563b87e323e5f0b54f667eba7532f10caa0eb3'
+else
+  Dummy::Application.config.secret_token = 'e40912e44be993f30695413b9afe02d1bf7a6f32a6a4a59ee8a414bcf6641c66a5f07371133f22fc268ae029bf563b87e323e5f0b54f667eba7532f10caa0eb3'
+end
